199aacd79fd89865319f00cfac7a755c11b6df71
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-05-25 19:36:04 +0200

    next commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+
+
+const about = () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+const contact = () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue')
+
+
+
 const routes = [
   {
     path: '/',
@@ -13,7 +20,17 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: about
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: contact,
+    props: {
+      title: 'Contact',
+      test: 'Some test value',
+    }
   }
 ]
 
---
+++ src/views/Contact.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <div>{{ text }}</div>
+        <div>ščťžýáíéŇŤŘĎČŠÓÍÚ</div>
+        <div>{{ test }}</div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        props: {
+            test: {
+                type: String,
+                default: "Model prop default",
+            },
+            title: {
+                type: String,
+                default: 'Prop title default value'
+            },
+        },
+        data() {
+            return {
+                text: 'This is some text form component variable.'
+            }
+        },
+    }
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
